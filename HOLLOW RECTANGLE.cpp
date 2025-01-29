c9098dff81c33500c25c4a82787c9ad6edf38ac2
IMDAD ULLAH <iuk23023@gmail.com>
2025-01-29 18:42:39 +0500

    Add files via upload

---
+++ HOLLOW RECTANGLE.cpp
@@ -0,0 +1,6 @@
+﻿Build started at 4:39 PM...
+1>------ Build started: Project: projrct 11, Configuration: Debug x64 ------
+1>HOLLOW RECTANGLE PATTERN.cpp
+1>projrct 11.vcxproj -> C:\Users\Imdad Ullah\source\repos\projrct 11\x64\Debug\projrct 11.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 4:39 PM and took 02.572 seconds ==========
