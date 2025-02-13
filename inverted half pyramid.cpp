22ce48d4f6ace34c7dbd7496d76525d155373197
IMDAD ULLAH <iuk23023@gmail.com>
2025-02-13 09:10:26 +0500

    Add files via upload

---
+++ inverted half pyramid.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+  using namespace std;
+int main(){
+	int n;
+	int num=1;
+cout<<"Enter you desired numbers to achieve:";
+	cin>>n;
+	for(int n=1;i<=n;i++){
+		//inner
+		for(int n=1;i<=i;i++){
+			cout<<num;
+			num++;
+		}
+		cout<<endl;
+	}
+	return 0;
+}
